b8e1cddec5fa675807096dc7b1676eebe1923ec1
kcavatar <kcavatar@kcavatar.in>
2018-05-12 10:47:32 +0530

    Added oblique shock module and main file

---
+++ main.py
@@ -0,0 +1,62 @@
+#*************************************************************************************#
+#                                                                                     #
+#                               SUPERSONIC FLOW OVER A CONE                           #
+#                                                                                     #
+#                            (solving Taylor-Maccoll equation)                        #
+#                                                                                     #
+#*************************************************************************************#
+
+# Description
+
+
+# Importing standard packages
+import matplotlib.pyplot
+
+
+
+# Importing user-defined packages
+from oblique_shock import *
+
+
+
+
+
+# Running computations
+
+
+
+
+
+
+
+
+# Checking
+
+
+
+
+
+
+
+
+
+# Writing to data files (preferably .vtk files)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ oblique_shock.py
@@ -0,0 +1,30 @@
+# Oblique shock wave properties
+
+import math
+
+def oblique_shock_wave_properties(M_1, beta, gamma):
+    
+    beta = beta*math.pi/180
+
+    # Flow deflection angle
+    tan_theta = (2/math.tan(beta))*(M_1**2*(math.sin(beta))**2 - 1)/(M_1**2*(gamma + math.cos(2*beta)) + 2) 
+    theta = math.atan(tan_theta)
+
+    # Normal components
+    M_n1 = M_1*math.sin(beta)
+    M_n2 = math.sqrt((M_n1**2*(gamma - 1) + 2)/(2*gamma*M_n1**2 - (gamma - 1)))
+    M_2 = M_n2/math.sin(beta - theta)
+
+    return [M_2, theta];
+
+
+if __name__ == "__main__": 
+   M_1 = float(input("Enter the value of freestream Mach number: "))
+   gamma = float(input("Enter the value of gamma: "))
+   beta = float(input("Enter the value of shock wave angle (in degrees): "))
+
+   [M_2, theta]  = oblique_shock_wave_properties(M_1, beta, gamma)
+   theta = theta*180/math.pi
+   print("M_2= " + str(M_2))
+   print("theta = " + str(theta))
+
