9bc1146d37fc16ebada94b3482122ff6183dae62
kcavatar <krishnaavatar77@hotmail.com>
2018-05-15 11:56:32 +0530

    Made minute changes to code

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ from taylor_maccoll import *
 
 #******************************** COMPUTATION ****************************************
 
-V_max = math.sqrt(C_p*T0) # Max theoretical speed
+V_max = math.sqrt(C_p*T0) # Max theoretical velocity (m/s)
 [M_2, delta, beta] = oblique_shock_wave_properties(M_inf, beta, gamma)
 
 # Setting up initial conditions
@@ -42,20 +42,20 @@ V_theta = [V*math.sin(beta - delta)]
 # Starting computations
 del_theta = 10**-5
 err = 1.0
-tol = 10.0**-10
+tol = 10.0**-8
 
 theta = [beta]
 i = 0
 
 print("Starting computation")
 while err>tol:
-      theta = theta + [beta - i*del_theta]
       [V_r_local, V_theta_local] = euler_step_integration(theta[i], del_theta, gamma, V_r[i], V_theta[i])
       V_theta += [V_theta_local]
       V_r += [V_r_local]
       err = V_theta[i]
       print(err)
       i += 1
+      theta = theta + [beta - i*del_theta]
       print("Ran incremental step : "+str(i))
       
 print("Semi-apex angle of the cone is "+ str(theta[-1]*180/math.pi))
